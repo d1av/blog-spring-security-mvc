890eb397d09389f6349b36904bc6d45b52dc2849
d1av <davi4alves@gmail.com>
2023-01-02 19:20:07 -0300

    feat: database simple seed

---
+++ src/main/resources/import.sql
@@ -0,0 +1,7 @@
+INSERT INTO Posts(title,content,description) VALUES ('My First Post','This is my first post','First Post Description');
+INSERT INTO Posts(title,content,description) VALUES ('My Second Post','This is my Second post','Second Post Description');
+
+INSERT INTO Comment(name,email,body,post_id) VALUES ('Comment 1','john@cena','First Comment',1);
+INSERT INTO Comment(name,email,body,post_id) VALUES ('Nearly first :(','john@cena','Close',1);
+INSERT INTO Comment(name,email,body,post_id) VALUES ('Comment 1','tony@stark','First Comment',2);
+INSERT INTO Comment(name,email,body,post_id) VALUES ('Nearly First','john@cena','Close',2);
