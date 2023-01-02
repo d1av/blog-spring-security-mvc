134642f12334e422f65a60325b4344a4352c8a73
d1av <davi4alves@gmail.com>
2023-01-02 20:07:43 -0300

    feat: add validation error

--- src/main/java/io/d1av/blog/exception/GlobalExceptionHandler.java
+++ src/main/java/io/d1av/blog/exception/GlobalExceptionHandler.java
@@ -1,16 +1,22 @@
 package io.d1av.blog.exception;
 
 import io.d1av.blog.payload.ErrorDetails;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 
 @ControllerAdvice
-public class GlobalExceptionHandler {
+public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
 
     @ExceptionHandler(ResourceNotFoundException.class)
     public ResponseEntity<ErrorDetails> handleResouceNotFound(ResourceNotFoundException exception,
@@ -25,7 +31,7 @@ public class GlobalExceptionHandler {
 
     @ExceptionHandler(BlogAPIException.class)
     public ResponseEntity<ErrorDetails> handleBlogAPI(BlogAPIException exception,
-                                                              WebRequest webRequest) {
+                                                      WebRequest webRequest) {
         ErrorDetails errorDetails = new ErrorDetails(
                 new Date(),
                 exception.getMessage(),
@@ -34,10 +40,10 @@ public class GlobalExceptionHandler {
         return new ResponseEntity<>(errorDetails, HttpStatus.BAD_REQUEST);
     }
 
-//global handler
+    //global handler
     @ExceptionHandler(Exception.class)
     public ResponseEntity<ErrorDetails> handleGlobalException(Exception exception,
-                                                      WebRequest webRequest) {
+                                                              WebRequest webRequest) {
         ErrorDetails errorDetails = new ErrorDetails(
                 new Date(),
                 exception.getMessage(),
@@ -46,4 +52,18 @@ public class GlobalExceptionHandler {
         return new ResponseEntity<>(errorDetails, HttpStatus.INTERNAL_SERVER_ERROR);
     }
 
+    @Override
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex,
+                                                                  HttpHeaders headers,
+                                                                  HttpStatusCode status,
+                                                                  WebRequest request) {
+        Map<String, String> errors = new HashMap<>();
+        ex.getBindingResult().getAllErrors().forEach(
+                (error) -> {
+                    String fieldName = error.getObjectName();
+                    String message = error.getDefaultMessage();
+                    errors.put(fieldName, message);
+                });
+        return new ResponseEntity<>(errors, HttpStatus.BAD_REQUEST);
+    }it c
 }
