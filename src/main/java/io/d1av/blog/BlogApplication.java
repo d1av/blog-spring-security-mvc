2a1cbf4688f1637b61fcf46fb19e59079e16ee61
d1av <davi4alves@gmail.com>
2023-01-02 15:59:36 -0300

    chore: initial commit from spring initializer

---
+++ src/main/java/io/d1av/blog/BlogApplication.java
@@ -0,0 +1,13 @@
+package io.d1av.blog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlogApplication.class, args);
+	}
+
+}
