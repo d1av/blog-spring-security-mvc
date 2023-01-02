34927cfa10aa3e054c63a5434c00b93893e0517d
d1av <davi4alves@gmail.com>
2023-01-02 19:27:20 -0300

    feat: exception handling

---
+++ src/main/java/io/d1av/blog/exception/GlobalExceptionHandler.java
@@ -0,0 +1,25 @@
+package io.d1av.blog.exception;
+
+import io.d1av.blog.payload.ErrorDetails;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+
+import java.util.Date;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(ResourceNotFoundException.class)
+    public ResponseEntity<ErrorDetails> handleResouceNotFound(ResourceNotFoundException exception,
+                                                              WebRequest webRequest) {
+        ErrorDetails errorDetails = new ErrorDetails(
+                new Date(),
+                exception.getMessage(),
+                webRequest.getDescription(false)
+        );
+        return new ResponseEntity<>(errorDetails, HttpStatus.BAD_REQUEST);
+    }
+}
