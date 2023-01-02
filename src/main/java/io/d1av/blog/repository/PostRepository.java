548ca823d6cca66ed697d9dcb9c567620abedde6
d1av <davi4alves@gmail.com>
2023-01-02 16:22:08 -0300

    feat: custom exception class

--- src/main/java/io/d1av/blog/repository/PostRepository.java
+++ src/main/java/io/d1av/blog/repository/PostRepository.java
@@ -2,8 +2,6 @@ package io.d1av.blog.repository;
 
 import io.d1av.blog.entity.Post;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
-@Repository
 public interface PostRepository extends JpaRepository<Post, Long> {
 }
