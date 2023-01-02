42293b878e42c5e9332d333d2c478c38cd0596dc
d1av <davi4alves@gmail.com>
2023-01-02 16:14:29 -0300

    feat: add post repository and entity

---
+++ src/main/java/io/d1av/blog/entity/Post.java
@@ -0,0 +1,59 @@
+package io.d1av.blog.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "posts", uniqueConstraints = @UniqueConstraint(columnNames = {"title"}))
+public class Post {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(name = "title", nullable = false)
+    private String title;
+    @Column(name = "description", nullable = false, length = 1000)
+    private String description;
+    @Column(name = "content", nullable = false)
+    private String content;
+
+    public Post() {
+    }
+
+    public Post(Long id, String title, String description, String content) {
+        this.id = id;
+        this.title = title;
+        this.description = description;
+        this.content = content;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+}
---
+++ src/main/java/io/d1av/blog/repository/PostRepository.java
@@ -0,0 +1,9 @@
+package io.d1av.blog.repository;
+
+import io.d1av.blog.entity.Post;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PostRepository extends JpaRepository<Post, Long> {
+}
