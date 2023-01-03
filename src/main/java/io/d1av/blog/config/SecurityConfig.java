1bc11a8c67cb5a1a8df0822f1803caaa1d70b2a5
d1av <davi4alves@gmail.com>
2023-01-03 01:12:23 -0300

    feat: add jwt in security config security chain

--- src/main/java/io/d1av/blog/config/SecurityConfig.java
+++ src/main/java/io/d1av/blog/config/SecurityConfig.java
@@ -1,13 +1,17 @@
 package io.d1av.blog.config;
 
+import io.d1av.blog.security.JwtAuthenticationEntryPoint;
+import io.d1av.blog.security.JwtAuthenticationFilter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -16,13 +20,20 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 public class SecurityConfig {
 
-    @Autowired
     private UserDetailsService userDetailsService;
+    private JwtAuthenticationEntryPoint authenticationEntryPoint;
+    private JwtAuthenticationFilter authenticationFilter;
 
+    public SecurityConfig(UserDetailsService userDetailsService, JwtAuthenticationEntryPoint authenticationEntryPoint, JwtAuthenticationFilter authenticationFilter) {
+        this.userDetailsService = userDetailsService;
+        this.authenticationEntryPoint = authenticationEntryPoint;
+        this.authenticationFilter = authenticationFilter;
+    }
 
     @Bean
     public static PasswordEncoder passwordEncoder() {
@@ -40,12 +51,15 @@ public class SecurityConfig {
                 .headers(header -> header.frameOptions().disable())
                 .authorizeHttpRequests(auth ->
                                 auth.requestMatchers("/api/auth/**").permitAll()
-                                        .requestMatchers( "/h2/**").permitAll()
+                                        .requestMatchers("/h2/**").permitAll()
                                         .anyRequest().authenticated()
                         // auth.requestMatchers(HttpMethod.GET, "/api/**").permitAll()
+                ).exceptionHandling(exception ->
+                        exception.authenticationEntryPoint(authenticationEntryPoint)
+                ).sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 );
         //   .exceptionHandling(x->x.accessDeniedHandler(accessDeniedHandler()));
-
+        http.addFilterBefore(authenticationFilter, UsernamePasswordAuthenticationFilter.class);
         return http.build();
     }
 
