670f9a2c0ff44581727473dac2eeec2567c5d280
d1av <davi4alves@gmail.com>
2023-01-02 23:00:07 -0300

    feat: add user from the database

--- src/main/java/io/d1av/blog/config/SecurityConfig.java
+++ src/main/java/io/d1av/blog/config/SecurityConfig.java
@@ -1,10 +1,14 @@
 package io.d1av.blog.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -16,17 +20,27 @@ import org.springframework.security.web.SecurityFilterChain;
 @Configuration
 public class SecurityConfig {
 
+    @Autowired
+    private UserDetailsService userDetailsService;
+
+
     @Bean
     public static PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
 
     @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration configuration) throws Exception {
+        return configuration.getAuthenticationManager();
+    }
+
+    @Bean
     SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http.csrf().disable()
+                .headers(header->header.frameOptions().disable())
                 .authorizeHttpRequests(auth ->
-                        auth.anyRequest().authenticated()
-                       // auth.requestMatchers(HttpMethod.GET, "/api/**").permitAll()
+                                auth.anyRequest().authenticated()
+                        // auth.requestMatchers(HttpMethod.GET, "/api/**").permitAll()
                         //        .requestMatchers( "/h2/**").permitAll()
                 ).httpBasic(Customizer.withDefaults());
         //   .exceptionHandling(x->x.accessDeniedHandler(accessDeniedHandler()));
@@ -34,21 +48,21 @@ public class SecurityConfig {
         return http.build();
     }
 
-    @Bean
-    public UserDetailsService userDetailsService() {
-        UserDetails ramesh = User.builder()
-                .username("ramesh")
-                .password(passwordEncoder().encode("ramesh"))
-                .roles("USER")
-                .build();
-
-        UserDetails admin = User.builder()
-                .username("admin")
-                .password(passwordEncoder().encode("admin"))
-                .roles("ADMIN")
-                .build();
-
-        return new InMemoryUserDetailsManager(ramesh, admin);
-    }
+//    @Bean
+//    public UserDetailsService userDetailsService() {
+//        UserDetails ramesh = User.builder()
+//                .username("ramesh")
+//                .password(passwordEncoder().encode("ramesh"))
+//                .roles("USER")
+//                .build();
+//
+//        UserDetails admin = User.builder()
+//                .username("admin")
+//                .password(passwordEncoder().encode("admin"))
+//                .roles("ADMIN")
+//                .build();
+//
+//        return new InMemoryUserDetailsManager(ramesh, admin);
+//    }
 }
 
--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -5,3 +5,10 @@ INSERT INTO Comment(name,email,body,post_id) VALUES ('Comment 1','john@cena','Fi
 INSERT INTO Comment(name,email,body,post_id) VALUES ('Nearly first :(','john@cena','Close',1);
 INSERT INTO Comment(name,email,body,post_id) VALUES ('Comment 1','tony@stark','First Comment',2);
 INSERT INTO Comment(name,email,body,post_id) VALUES ('Nearly First','john@cena','Close',2);
+
+INSERT INTO roles(id,name) VALUES (1,'ROLE_USER'),(2,'ROLE_ADMIN');
+
+INSERT INTO users(email,name,password,username) VALUES ('admin@admin','Admin','$2a$10$FUlO0odzUuxuv1KbGjOgE.Kr9Xuz3baIMsVlhTENkD9QPkB.ecRiK','admin');
+INSERT INTO users(email,name,password,username) VALUES ('davi@davi','Davi','$2a$10$9B7YovC4owgOeiHTTny.PucbEx3pWX4KAEgX447vnOpjcfguLqsB6','davi');
+
+INSERT INTO users_roles(user_id,role_id) VALUES (1,1),(1,2),(2,1);
