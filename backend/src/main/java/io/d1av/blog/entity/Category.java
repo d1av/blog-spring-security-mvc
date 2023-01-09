79eaf16e372ff728ea7f26a4138b8f508a7294df
d1av <davi4alves@gmail.com>
2023-01-09 20:37:00 -0300

    feat: add category entity

---
+++ backend/src/main/java/io/d1av/blog/entity/Category.java
@@ -0,0 +1,47 @@
+package io.d1av.blog.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "categories")
+public class Category {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String description;
+
+    public Category() {
+    }
+
+    public Category(Long id, String name, String description) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
---
+++ backend/src/main/java/io/d1av/blog/repository/CategoryRepository.java
@@ -0,0 +1,6 @@
+package io.d1av.blog.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CategoryRepository extends JpaRepository<Category,Long> {
+}
